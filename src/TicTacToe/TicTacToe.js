66c535a8af745efa4192fae82e8231b216ec9048
Davner <davner23@gmail.com>
2022-06-20 03:05:43 -0400

    Criação do jogo da velha - 20/06/2022

---
+++ src/TicTacToe/TicTacToe.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react'
+import './TicTacToe.css'
+
+const TicTacToe = () => {
+
+    const [turn, setTurn] = useState('x');
+    const handleClick = (num) => {
+        alert(num);
+    }
+    const Cell = ({num}) => {
+        return <td onClick={() => handleClick(num)}>-</td>
+    }
+
+  return (
+    <div className="container">
+        <table>
+            Turn: {turn}
+            <tbody>
+                <tr>
+                    <Cell num={0} />
+                    <Cell num={1} />
+                    <Cell num={2} />
+                </tr>
+                <tr>
+                    <Cell num={3} />
+                    <Cell num={4} />
+                    <Cell num={5} />
+                </tr>
+                <tr>
+                    <Cell num={6} />
+                    <Cell num={7} />
+                    <Cell num={8} />
+                </tr>
+            </tbody>
+        </table>
+    </div>
+  );
+}
+
+export default TicTacToe;
