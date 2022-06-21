8eb7f9dbfd7a8b918cf9f002efa0c69648fd94b5
Davner <davner23@gmail.com>
2022-06-21 14:56:08 -0400

    Ajustes no jogo - 21/06/2022

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@
 import './App.css';
 import TicTacToe from './TicTacToe/TicTacToe';
 import Navbar from './TicTacToe/navbar';
+import Footer from './TicTacToe/footer';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <TicTacToe />
+      <Footer />
     </div>
   );
 }
---
+++ src/TicTacToe/footer.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './TicTacToe.css';
+
+const footer = () => {
+  return (
+    <div>
+      <div className="footer">
+        <div className="social-media">
+            <a href="https://www.instagram.com">Instagram</a>
+            <a href="https://www.facebook.com">Facebook</a>
+            <a href="https://www.twitter.com">Twitter</a>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default footer;
